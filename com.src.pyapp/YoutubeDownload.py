c4e88d8bb6f2b0cd0daffb58365ae701eb5b87e4
gburnama <blackprimenoz@gmail.com>
2023-03-27 22:13:36 +0700

    add youtube video download for test1

--- com.src.pyapp/YoutubeDownload.py
+++ com.src.pyapp/YoutubeDownload.py
@@ -1,29 +1,52 @@
-from pytube import YouTube
-import moviepy.editor as mp
 import os
+import tkinter as tk
+from tkinter import filedialog
+from pytube import YouTube
 
-# function to download YouTube video
-def download_video(url, path):
-    yt = YouTube(url)
-    stream = yt.streams.filter(only_audio=True).first()
-    stream.download(output_path=path, filename=yt.title+ '.mp4')
-    print("Video downloaded successfully.")
+class YoutubeDownloaderGUI:
+    def __init__(self, master):
+        self.master = master
+        master.title("Youtube Downloader")
 
-# function to convert video to MP3
-def convert_to_mp3(path):
-    video = mp.VideoFileClip(path)
-    mp3_path = os.path.splitext(path)[0] + ".mp3"
-    video.audio.write_audiofile(mp3_path)
-    print("Conversion to MP3 complete.")
+        self.label_url = tk.Label(master, text="Enter the YouTube video URL:")
+        self.label_url.pack()
 
-# download and convert video
-if __name__ == '__main__':
-    url = input("Enter the YouTube video URL : ")
-    path = input("Enter the output path: ")
+        self.entry_url = tk.Entry(master, width=50)
+        self.entry_url.pack()
 
-    # download video
-    download_video(url, path)
+        self.label_path = tk.Label(master, text="Select output directory:")
+        self.label_path.pack()
 
-    # convert to MP3
-    video_path = os.path.join(path, YouTube(url).title + '.mp4')
-    convert_to_mp3(video_path)
+        self.btn_path = tk.Button(master, text="Select Directory", command=self.select_directory)
+        self.btn_path.pack()
+
+        self.label_status = tk.Label(master, text="")
+        self.label_status.pack()
+
+        self.btn_download = tk.Button(master, text="Download", command=self.download_video)
+        self.btn_download.pack()
+
+    def select_directory(self):
+        self.directory = filedialog.askdirectory(initialdir=os.getcwd(), title="Select directory")
+        self.label_status.config(text="Selected directory: " + self.directory)
+
+    def download_video(self):
+        url = self.entry_url.get().strip()
+        path = self.directory
+
+        if url == "":
+            self.label_status.config(text="Error: Please enter a valid YouTube video URL.")
+            return
+        if path is None:
+            self.label_status.config(text="Error: Please select an output directory.")
+            return
+
+        try:
+            YouTube(url).streams.first().download(path)
+            self.label_status.config(text="Video downloaded successfully.")
+        except Exception as e:
+            self.label_status.config(text="Error: " + str(e))
+
+root = tk.Tk()
+gui = YoutubeDownloaderGUI(root)
+root.mainloop()
